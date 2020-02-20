c2b945ee4a713174b83bb45d60527195af1dafd4
@higakikeita <higaki.keichan@gmail.com>
2020-02-20 14:24:23 +0900

    likesコントローラーの設定

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :users, only: [:index, :show]
   resources :posts, only: [:index, :show, :create]
-
+    resources :likes, only: [:create, :destroy]
   root 'posts#index'
 
 end
